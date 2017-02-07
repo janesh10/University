18c9c54d9e64671fa3b78797a1d3d36b052f9ed1
Janesh Chudasama <P1413434X@GWHXX680WIN020W.LEC-ADMIN.dmu.ac.uk>
2017-02-07 15:25:49 +0000

    functions

---
+++ functions.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	
+	
+	int x=20;
+	int y=10;
+	int j;
+	int k;
+
+	
+	//Addtion Function
+	printf("%i + %i =%i\n",x,y,x+y);
+
+	//Subtraction Function
+	printf("%i - %i =%i\n",x,y,x-y);
+
+	//Division function
+	printf("%i / %i =%i\n",x,y,x/y);
+	//Multiplication function
+	printf("%i * %i =%i\n",x,y,x*y);
+
+
+
+	
+	
+	return 0;
+}
